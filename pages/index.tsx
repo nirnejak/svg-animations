40e256d99cf616d55b55b9f04f2c93e6a1dab500
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-08 21:32:20 +0530

    Home: boilerplate code removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,8 @@
 import * as React from "react"
-import { useInView } from "react-intersection-observer"
 
-import { motion, useAnimation } from "framer-motion"
 import Head from "next/head"
 
 const Home = () => {
-  const controls = useAnimation()
-  const [ref, inView] = useInView()
-
-  React.useEffect(() => {
-    if (inView) {
-      controls.start("visible").catch((err) => {
-        console.log(err)
-      })
-    }
-  }, [controls, inView])
-
-  const variants = {
-    visible: { opacity: 1, translateY: 0 },
-    hidden: { opacity: 0, translateY: 10 },
-  }
-
   return (
     <div>
       <Head>
@@ -29,18 +11,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <motion.main
-        animate={controls}
-        initial="hidden"
-        transition={{ delay: 0, duration: 0.15, type: "spring" }}
-        variants={variants}
-        ref={ref}
-        className="grid h-screen place-content-center"
-      >
-        <h1 className="text-center text-8xl font-bold text-slate-800">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-      </motion.main>
+      <main className="grid min-h-screen grid-cols-3"></main>
     </div>
   )
 }
