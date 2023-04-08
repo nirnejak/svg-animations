5c65d5821880fc2cb9ca4730c76319b434b69b85
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-08 22:10:09 +0530

    GridItem: unused div removed

--- components/GridItem.tsx
+++ components/GridItem.tsx
@@ -1,9 +1,7 @@
 import * as React from "react"
 
 const GridItem: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <div className="border-[0.5px] p-10">
-    <div>{children}</div>
-  </div>
+  <div className="border-[0.5px] p-10">{children}</div>
 )
 
 export default GridItem
