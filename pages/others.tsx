a752d1819f2e799ba1eda9e7ca5ce6fbd3b605dc
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-13 02:06:17 +0530

    Other illustrations in /details and /others page

--- pages/others.tsx
+++ pages/others.tsx
@@ -3,13 +3,8 @@ import * as React from "react"
 import Head from "next/head"
 
 import GridItemWrapper from "components/GridItemWrapper"
-import DataChangeAlertIllustration from "components/illustration/DataChangeAlert"
-import DataSourceConnectionIllustration from "components/illustration/DataSourceConnection"
-import DataThresholdAlertIllustration from "components/illustration/DataThresholdAlert"
 import DraxlrInSlackIllustration from "components/illustration/DraxlrInSlack"
-import GroupDashboardIllustration from "components/illustration/GroupDashboard"
 import SetAlertsIllustration from "components/illustration/SetAlerts"
-import TimelyUpdatesIllustration from "components/illustration/TimelyUpdates"
 
 const Other: React.FC = () => {
   return (
@@ -26,43 +21,6 @@ const Other: React.FC = () => {
         </GridItemWrapper>
         <GridItemWrapper>
           <DraxlrInSlackIllustration />
-        </GridItemWrapper>
-      </main>
-      <main className="grid grid-cols-3">
-        <GridItemWrapper>
-          <div className="flex">
-            <div className="m-auto">
-              <DataSourceConnectionIllustration />
-            </div>
-          </div>
-        </GridItemWrapper>
-        <GridItemWrapper>
-          <div className="flex">
-            <div className="m-auto">
-              <DataChangeAlertIllustration />
-            </div>
-          </div>
-        </GridItemWrapper>
-        <GridItemWrapper>
-          <div className="flex">
-            <div className="m-auto">
-              <DataThresholdAlertIllustration />
-            </div>
-          </div>
-        </GridItemWrapper>
-        <GridItemWrapper>
-          <div className="flex">
-            <div className="m-auto">
-              <TimelyUpdatesIllustration />
-            </div>
-          </div>
-        </GridItemWrapper>
-        <GridItemWrapper>
-          <div className="flex">
-            <div className="m-auto">
-              <GroupDashboardIllustration />
-            </div>
-          </div>
         </GridItemWrapper>
       </main>
     </div>
