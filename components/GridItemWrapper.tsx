00488264f55735edf2180e514f57d070f2484956
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2023-04-22 03:58:26 +0530

    GridWrapper style changed

--- components/GridItemWrapper.tsx
+++ components/GridItemWrapper.tsx
@@ -3,7 +3,7 @@ import * as React from "react"
 const GridItemWrapper: React.FC<{ children: React.ReactNode }> = ({
   children,
 }) => (
-  <div className="relative flex flex-col items-center justify-center border-[0.5px] p-10">
+  <div className="relative flex flex-col items-center justify-center rounded-3xl bg-zinc-50 p-10">
     {children}
   </div>
 )
