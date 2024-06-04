56026abee8f66b346d686d4947615cf4a9b9d428
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-06-05 04:22:42 +0530

    Footer: responsiveness fixes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,7 +8,7 @@ const Footer: React.FC = () => {
       initial={{ opacity: 0, translateY: 50, translateX: "-50%" }}
       animate={{ opacity: 1, translateY: 0, translateX: "-50%" }}
       transition={{ delay: 3 }}
-      className="fixed bottom-5 left-1/2 rounded-md bg-white px-3 py-2 text-sm text-gray-700 shadow-lg"
+      className="fixed bottom-5 left-1/2 w-[calc(100vw-30px)] -translate-x-1/2 rounded-md bg-white px-3 py-2 text-center text-sm text-gray-700 shadow-lg md:w-max"
     >
       Designed in <strong>Figma</strong>, animated with{" "}
       <strong>Framer Motion</strong> by{" "}
