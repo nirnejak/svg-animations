c9201962602ea5fc135a606ddbe9d478a299a1da
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2024-10-24 19:43:12 +0530

    global config file added

---
+++ config.ts
@@ -0,0 +1,12 @@
+const config = {
+  baseUrl: "https://svg-animations-gallery.vercel.app",
+  appName: "SVG Animations",
+  appDescription: "SVG Animations gallery built with Framer Motion",
+  creator: "Jitendra Nirnejak",
+  authorName: "Jitendra Nirnejak",
+  authorUrl: "<Author Name or Email>",
+  keywords: ["SVG", "Animations", "Framer Motion", "Next.js", "TypeScript"],
+  twitterSite: "@nirnejak",
+  twitterCreator: "@nirnejak",
+}
+export default config
