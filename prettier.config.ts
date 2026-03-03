7b684d98ce777b17d06e2ad2bbb8eb75ef083c62
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2026-03-03 10:35:53 +0530

    prettier and eslint config removed

--- prettier.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from "prettier"
-import * as tailwindConfig from "prettier-plugin-tailwindcss"
-
-const config: Config = {
-  plugins: [tailwindConfig],
-  tailwindFunctions: ["classNames"],
-}
-
-export default config
